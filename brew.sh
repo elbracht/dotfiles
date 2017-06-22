69304980ba05ac16853a8697ce8064a0b6b50621
Alexander Elbracht <alexander@elbracht.org>
2017-06-22 15:56:42 +0200

    Install Atom and Fira Code with brew

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v brew > /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap caskroom/fonts
+brew update
+brew upgrade
+
+brew cask install atom
+
+brew cask install font-fira-code
